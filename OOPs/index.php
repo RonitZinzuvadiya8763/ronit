a95ce5e6f099b44f46fdf6cab312848ff9fcd1b3
Ronit Zinzuvadiya <ronit@rankwaytechnology.com>
2023-05-18 10:25:24 +0530

    singleInheritance

--- OOPs/index.php
+++ OOPs/index.php
@@ -1,43 +1,6 @@
 <?php
-    class TestClass
-    {
-        public $name;
-        function __construct()
-        {
-            echo "hello world";
-        }
-        function __destruct() 
-        {
-            echo "destruct {$this->name}";
-        }
-        function welcome()
-        {
-            return "welcome";
-        }
-
-        function getName()
-        {
-            if (!isset($this->name)) {
-                return "No name";
-            } else {
-                return $this->name;
-            }
-        }
-
-        function setName($name)
-        {
-            $this->name = $name;
-            echo "successfully set";
-        }
-    }
-        $testObj = new TestClass();
-        echo "<br>";
-        echo $testObj->welcome();
-        echo "<br>";
-        // echo $testObj->getName();
-        // echo "<br>";
-        $testObj->setName('Ronit');
-        echo "<br>";
-        echo $testObj->getName();
-        echo "<br>";
-?>
+// echo "hi";
+// exit;
+include("inheriance.php");
+exit;
+include("basic-oop.php");
---
+++ OOPs/inheriance.php
@@ -0,0 +1,14 @@
+<?php
+    class Parent{
+
+    }
+
+    class Child{
+        
+    }
+
+    $testObj = new Parent();
+        echo "<br>";
+        echo $testObj->welcome();
+
+?>
