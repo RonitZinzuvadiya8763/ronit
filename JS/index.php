a14cb82ff2e26cc6bff9a6e74777b253d9d09d31
Ronit Zinzuvadiya <ronit@rankwaytechnology.com>
2023-05-23 18:18:52 +0530

    php array training

---
+++ JS/index.php
@@ -0,0 +1,21 @@
+<html>
+<h2>JavaScript</h2>
+
+Name: <input type="text" id="name" name="name"><br>
+Phone: <input type="number" id="phone" name="phone"><br>
+Message: <input type="textarea" id="msg" name="msg"><br><br>
+<button onclick="change_text()">Submit</button>
+
+<p id="demo"></p>
+
+<script>
+  function change_text() {
+    var name = document.getElementById("name").value;
+    var phone = document.getElementById("phone").value;
+    var msg = document.getElementById("msg").value;
+
+    document.getElementById("demo").innerHTML = 'Name: ' +  name + '<br>' + 'Phone: ' + phone + '<br>' + 'Message: ' + msg;
+  }
+</script>
+
+</html>
