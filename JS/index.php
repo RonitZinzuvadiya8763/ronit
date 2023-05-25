45d801ef7918065dfc99ca2a4e95dd37489a5e98
Ronit Zinzuvadiya <ronit@rankwaytechnology.com>
2023-05-25 13:27:13 +0530

    php js training

--- JS/index.php
+++ JS/index.php
@@ -8,24 +8,27 @@ Message: <input type="textarea" id="msg" name="msg"><br><br>
 
 <!-- <button onclick="displayDate()">Click to view date and time</button> -->
 
-<p id="demo"></p>
-<p id="demo1"></p>
+ <p id="demo"></p>
+<!-- <p id="demo1"></p>
 <p id="demo2"></p>
-<p id="demo3"></p>
+<p id="demo3"></p>  -->
 
 <!-- <script type="text/javascript" src="index.js"> -->
 <script>
+// console.log("hi");
+// exit;
+document.getElementById("demo").innerHTML = "Hello World!";
+  // var surname = 'hello';
+  // function getName() {
+  //   let name = "ABC";
+  //   surname = 'hello1';
+  // }
 
-  
-  var surname = 'hello';
-  function getName() {
-    let name = "ABC";
-    surname = 'hello1';
-  }
-
-  console.log(name);
-  // getName();
-  console.log(surname);
+  // console.log(name);
+  // // getName();
+  // // debugger
+  // // display in sources
+  // console.log(surname);
 
 
 </script>
