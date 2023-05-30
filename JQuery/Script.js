f339aeef782b04b2573318381256bae79f3e371d
Ronit Zinzuvadiya <ronit@rankwaytechnology.com>
2023-05-30 18:34:32 +0530

    Calculator

--- JQuery/Script.js
+++ JQuery/Script.js
@@ -185,11 +185,36 @@
 // });
 
 // Topic: JQuery Filers (Filter Lists)
-$(document).ready(function(){
-  $("#myInput").on("keyup", function() {
-    var value = $(this).val().toLowerCase();
-    $("#myList li").filter(function() {
-      $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
-    });
+// $(document).ready(function(){
+//   $("#myInput").on("keyup", function() {
+//     var value = $(this).val().toLowerCase();
+//     $("#myList li").filter(function() {
+//       $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+//     });
+//   });
+// });
+
+
+// Topic: Calculator
+$(document).ready(function () {
+  $(".numBtn").click(function () {
+    var value1 = $(this).val();
+    var value2 = $("#input").val();
+    $("#input").val(value2 + value1);
+  });
+  
+  $(".clrBtn").click(function () {
+    $("#input").val("");
+  });
+
+  $(".opBtn").click(function () {
+    var value1 = $(this).val();
+    var value2 = $("#input").val();
+    $("#input").val(value2 + value1);
+  });
+
+  $(".calBtn").click(function () {
+    var value2 = eval($("#input").val());
+    $("#input").val(value2);
   });
 });
