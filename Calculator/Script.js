b29353e22f37ae24865bab168b1424309f0dd19a
Ronit Zinzuvadiya <ronit@rankwaytechnology.com>
2023-06-01 16:36:26 +0530

    Update Script.js

--- Calculator/Script.js
+++ Calculator/Script.js
@@ -1,69 +1,61 @@
 $(document).ready(function () {
-  $(".numBtn").click(function () {
+  $(".numBtn, .opBtn").click(function () {
     var value1 = $(this).val();
     var value2 = $("#input").val();
     $("#input").val(value2 + value1).focus();
   });
 
-  // $("#input").keypress(function valid_numbers(e)
-  // {
-  //       var key=e.which || e.KeyCode;
-  //       if  ( key >=48 && key <= 57)
-  //        // to check whether pressed key is number or not 
-  //         return true; 
-  //       else 
-  //         return false;
-  // });
-
-  // $(".numtxt").on("input", function(evt) {
-  //   var self = $(this);
-  //   self.val(self.val().replace(/[^0-9\.]/g, ''));
-  //   if ((evt.which != 46 || self.val().indexOf('.') != -1) && (evt.which < 48 || evt.which > 57)) 
-  //   {
-  //     evt.preventDefault();
-  //   }
-  // });
-
-  $(function () {
-    $(document).keyup(function (e) {
-      console.log(e.keyCode);
-    });
-  });
-
   $(".clrBtn").click(function () {
-    $("#input").val("");
+    $("#input").val("").focus();
   });
 
   $(".delBtn").click(function () {
-    var num = $("#input").val();
-    var deletelast = num / 10 | 0;
-    $("#input").val(deletelast);
-  });
-
-  $(".opBtn").click(function () {
-    var value1 = $(this).val();
-    var value2 = $("#input").val();
-    $("#input").val(value2 + value1);
+    let num = $("#input").val();
+    num = num.substring(0, num.length - 1);
+    $("#input").val(num);
   });
 
   $(".calBtn").click(function () {
-    var value2 = eval($("#input").val());
-    $("#input").val(value2);
-  });
-
-  $("#input").keypress(function (evt) {
-    if (evt.which == 13) {
-      $(".calBtn").trigger('click');
+    $arithmaticChecker = $("#input").val()[$("#input").val().length - 1];
+    if (
+      ($arithmaticChecker == '+') ||
+      ($arithmaticChecker == '-') ||
+      ($arithmaticChecker == '*') ||
+      ($arithmaticChecker == '/')
+    ) {
+      window.alert("Can not use operator in the ending !!!");
+    }
+    else {
+      let result = eval($("#input").val());
+      $("#input").val(result);
     }
   });
-  
-  $('#input').on('input', function () {
+
+  $(document).on('keypress', function (e) {
+    e.preventDefault();
     var value = $(this).val();
-    var regex = /^[0-9+\-*/.]+$/; //allow numbers and arithmetic operators
-
-    if (!regex.test(value)) {
-      $(this).val(value.replace(/[^0-9+\-*/.]/g, '')); // Remove invalid characters
+    if (e.which >= '42' && e.which <= "57") {
+      e.originalEvent.key
+      $('#input').val(
+        $('#input').val() + e.originalEvent.key
+      );
     }
+
+    if (e.which == 13) {
+      $arithmaticChecker = $("#input").val()[$("#input").val().length - 1];
+      if (
+        ($arithmaticChecker == '+') ||
+        ($arithmaticChecker == '-') ||
+        ($arithmaticChecker == '*') ||
+        ($arithmaticChecker == '/')
+      ) {
+        window.alert("Can not use operator in the ending !!!");
+      }
+      else {
+        $(".calBtn").trigger('click');
+      }
+    }
+
   });
 
 });
